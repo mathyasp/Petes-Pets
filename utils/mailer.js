8cc8f43bf934d110c762462620edf6fa5aface2d
mathyasp <mathyastp@gmail.com>
2024-06-13 08:12:33 -0400

    Implemented Emails

--- utils/mailer.js
+++ utils/mailer.js
@@ -10,21 +10,25 @@ const auth = {
 
 const nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
+// export our send mail function
 module.exports.sendMail = (user, req, res) => {
-    nodemailerMailgun.sendMail({
-        from: 'no-reply@example.com',
-        to: 'markms89@yahoo.com',
-        subject: 'Pet Purchased',
-        template: {
-            name: 'email.handlebars',
-            engine: 'handlebars',
-            context: user
-        }
-    }).then(info => {
-        console.log('Response: ', info);
-        res.redirect(`/pets/${req.params.id}`);
-    }).catch(err => {
-        console.log('mailsend error: ', err);
-        res.redirect(`/pets/${req.params.id}`);
-    });
+  // send an email to the user's email with a provided template
+  nodemailerMailgun.sendMail({
+      from: 'no-reply@example.com',
+      to: user.email, // An array if you have multiple recipients.
+      subject: 'Pet Purchased!',
+      template: {
+          name: 'email.handlebars',
+          engine: 'handlebars',
+          context: user
+      }
+  // One mail is sent, redirect to the purchased pet's page
+  }).then(info => {
+      console.log('Response: ' + info);
+      res.redirect(`/pets/${req.params.id}`);
+  // Catch error and redirect to the purchased pet's page
+  }).catch(err => {
+      console.log('Error: ' + err);
+      res.redirect(`/pets/${req.params.id}`);
+  });
 }
